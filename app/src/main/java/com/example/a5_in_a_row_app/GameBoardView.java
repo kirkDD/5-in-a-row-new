7e2146097a2d5fe0342cdfc6f2220f90e22423ac
kirkD <kirkdai111@gmail.com>
2020-06-19 15:47:30 -0700

    changed onTouchEvent state machine

--- app/src/main/java/com/example/a5_in_a_row_app/GameBoardView.java
+++ app/src/main/java/com/example/a5_in_a_row_app/GameBoardView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
+import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,11 @@ public class GameBoardView extends View {
     Drawable BLACK_PNG;
     Drawable WHITE_PNG;
 
+    // the current state of the board(selecting or not)
+    State state;
+    // the current location of selection on the board
+    Pair<Integer, Integer> location;
+
     public GameBoardView(Context context, FiveInARowGame game) {
 
         super(context);
@@ -35,6 +42,9 @@ public class GameBoardView extends View {
 
         BLACK_PNG = getResources().getDrawable(R.drawable.black_piece, null);
         WHITE_PNG = getResources().getDrawable(R.drawable.white_piece, null);
+
+        state = State.START;
+        location = null;
     }
 
     void initDims() {
@@ -45,22 +55,58 @@ public class GameBoardView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_UP:
-                registerMove(event.getX(), event.getY());
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_UP:
+//                registerMove(event.getX(), event.getY());
+//                return true;
+//            case MotionEvent.ACTION_MOVE:
+//                invalidate();
+//                return true;
+//            default:
+//                break;
+//        }
+//        return false;
+        //get location of the current selection on the board
+        Pair<Integer, Integer> current = essentialGeometry(new PointF(event.getX(), event.getY()));
+        switch(state) {
+            case START:
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    state = State.SELECTING;
+                    return true;
+                }
+            case SELECTING:
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    updateModel(current);
+                    invalidate();
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    registerMove(event.getX(), event.getY());
+                    invalidate();
+                    state = State.START;
+                    location = null;
+                    return true;
+                }
+                break;
             default:
-                return true;
+                break;
         }
+        return false;
     }
 
+    // updates the current location of selection
+    private void updateModel(Pair<Integer, Integer> p) {
+        if (p != location) {
+            location = p;
+        }
+    }
     void registerMove(float xPos, float yPos) {
         int x = (int) (xPos / tileSize);
         int y = (int) (yPos / tileSize);
         if (x < numTileOneSide && y < numTileOneSide) {
             game.makeMove(x, y, game.nextPlayer());
-            invalidate();
         }
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -68,15 +114,15 @@ public class GameBoardView extends View {
         // how to draw a board?
         // game != null
         int[][] board = game.getBoard();
+        brush.setStyle(Paint.Style.FILL);
+        brush.setColor(Color.GRAY);
+        canvas.drawRect(0, 0, size, size, brush);
+        brush.setColor(Color.DKGRAY);
+        brush.setStyle(Paint.Style.STROKE);
         for (int i = 0; i < numTileOneSide; i++) {
             for (int j = 0; j < numTileOneSide; j++) {
                 float x = i * tileSize;
                 float y = j * tileSize;
-                brush.setColor(Color.DKGRAY);
-                brush.setStyle(Paint.Style.STROKE);
-                canvas.drawRect(x, y, x + tileSize, y + tileSize, brush);
-                brush.setStyle(Paint.Style.FILL);
-                brush.setColor(Color.GRAY);
                 canvas.drawRect(x, y, x + tileSize, y + tileSize, brush);
                 if (board[i][j] == FiveInARowGame.BLACK) {
                     BLACK_PNG.setBounds((int) x+1, (int) y+1, (int) (x + tileSize) - 1, (int) (y + tileSize) - 1);
@@ -85,7 +131,20 @@ public class GameBoardView extends View {
                     WHITE_PNG.setBounds((int) x+1, (int) y+1, (int) (x + tileSize) - 1, (int) (y + tileSize) - 1);
                     WHITE_PNG.draw(canvas);
                 }
+                if (location != null && i == location.first && j == location.second) {
+                    if (game.nextPlayer() == FiveInARowGame.BLACK) {
+                        BLACK_PNG.setBounds((int) x+1, (int) y+1, (int) (x + tileSize) - 1, (int) (y + tileSize) - 1);
+                        BLACK_PNG.draw(canvas);
+                    } else if (game.nextPlayer() == FiveInARowGame.WHITE) {
+                        WHITE_PNG.setBounds((int) x+1, (int) y+1, (int) (x + tileSize) - 1, (int) (y + tileSize) - 1);
+                        WHITE_PNG.draw(canvas);
+                    }
+                }
             }
         }
+    }
+
+    public Pair<Integer, Integer> essentialGeometry(PointF p) {
+        return Pair.create((int) (p.x / tileSize), (int) (p.y / tileSize));
     }
 }
---
+++ app/src/main/java/com/example/a5_in_a_row_app/State.java
@@ -0,0 +1,6 @@
+package com.example.a5_in_a_row_app;
+
+public enum State {
+    START,
+    SELECTING
+}
